39ca32e8968cf7ef9faf5de5152b4351ee3c0275
Othmane OUAZZANI TOUHAMI <othmaneouazzanit@gmail.com>
2024-02-08 12:14:34 +0100

    Login UI and User Model

---
+++ DotNetTraining/Models/Role.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTraining.Models
+{
+    class Role
+    {
+        private string Name { get; set; }
+        private string Description { get; set; }
+        public Role() { }
+        public Role(string name, string description)
+        {
+            Name = name;
+            Description = description;
+
+        }
+
+    }
+}
---
+++ DotNetTraining/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTraining.Models
+{
+    class User
+    {
+        [Required]
+        private string Id { get; set; }
+        [Required]
+        private string FirstName { get; set; }
+        [Required]
+        private string LasttName { get; set; }
+        [Required]
+        private string Password { get; set; }
+        [Required]
+        private string Email { get; set; }
+
+        private Role role { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+       
+    }
+}
